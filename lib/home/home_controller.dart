3d7e018a8c3a3863391df6a2a308f1129eed7f0e
Hpasa <henriquepasa@hotmail.com>
2021-04-23 20:52:37 -0300

    Aula 03

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/home_repository.dart';
 import 'package:DevQuiz/home/home_state.dart';
 import 'package:DevQuiz/shared/models/awnser_model.dart';
 import 'package:DevQuiz/shared/models/question_model.dart';
@@ -15,50 +16,20 @@ class HomeController {
   UserModel? user;
   List<QuizModel>? quizzes;
 
+  final repository = HomeRepository();
+
   void getUser() async {
     setStateNotifier = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
 
-    user = UserModel(
-      name: "Henrique",
-      photoUrl: "https://avatars.githubusercontent.com/u/51676261?v=4",
-    );
+    user = await repository.getUser();
 
     setStateNotifier = HomeState.success;
   }
 
   void getQuizzes() async {
     setStateNotifier = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
 
-    quizzes = [
-      QuizModel(
-        title: "NLW5 Fluter",
-        imagem: AppImages.blocks,
-        questionAnswered: 1,
-        level: Level.facil,
-        questions: [
-          QuestionModel(
-            title: "Está curtindo o Fluter?",
-            awnsers: [
-              AwnserModel(title: "Pouco"),
-              AwnserModel(title: "Não muito"),
-              AwnserModel(title: "Tá Legal"),
-              AwnserModel(title: "Estou Amando", isRight: true)
-            ]
-          ),
-          QuestionModel(
-            title: "Está curtindo o Fluter?",
-            awnsers: [
-              AwnserModel(title: "Pouco"),
-              AwnserModel(title: "Não muito"),
-              AwnserModel(title: "Tá Legal"),
-              AwnserModel(title: "Estou Amando", isRight: true)
-            ]
-          )
-        ],
-      )
-    ];
+    quizzes = await repository.getQuizzes();
 
     setStateNotifier = HomeState.success;
   }
